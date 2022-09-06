e7633d6923bcf30b3255eb8c89bb3257d6db21cc
Наталья Бачурина <natali.gorobets.86@mail.ru>
2022-09-06 08:54:27 +0700

    added new tests in a new branch

--- src/test/java/ru/netology/OrderingCardDelivery.java
+++ src/test/java/ru/netology/OrderingCardDelivery.java
@@ -1,29 +1,53 @@
 package ru.netology;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.lang.module.Configuration;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class OrderingCardDelivery {
 
     @BeforeEach
     void test1() {
-        Configuration.holdBrowserOpen = true;
 
-        Selenide.open("http://localhost:9999/");
+        open("http://localhost:9999/");
 
     }
+
     public String getDate() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         Calendar calendar = new GregorianCalendar();
         calendar.add(Calendar.DAY_OF_MONTH, +5);
         return dateFormat.format(calendar.getTime());
+    }
+
+    @Test
+
+    void completedForm() {
+        Configuration.holdBrowserOpen = true;
+
+        $("[data-test-id=city] input").setValue("Красноярск");
+        $("[data-test-id=date] .input__control").click();
+        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=date] .input__control").sendKeys(BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(getDate());
+        $("[data-test-id=name] .input__control").setValue("Иван Васильев");
+        $("[data-test-id=phone] .input__control").setValue("+79135178898");
+        $("[data-test-id=agreement]").click();
+        $x("//button[contains(@class, 'button_view_extra')]").click();
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + getDate()));
     }
 
 }
