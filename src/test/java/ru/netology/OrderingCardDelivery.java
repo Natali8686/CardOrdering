87fefb1c466ba8ebc2ec509dcbef0473131f1093
Наталья Бачурина <natali.gorobets.86@mail.ru>
2022-09-07 10:07:39 +0700

    changing the method

--- src/test/java/ru/netology/OrderingCardDelivery.java
+++ src/test/java/ru/netology/OrderingCardDelivery.java
@@ -1,16 +1,12 @@
 package ru.netology;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
@@ -25,11 +21,9 @@ public class OrderingCardDelivery {
 
     }
 
-    public String getDate() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_MONTH, +5);
-        return dateFormat.format(calendar.getTime());
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));// метод принимает количество дней сдвига относительно текущей даты, а возвращает отформатированную строку с датой. Будет возможность генерировать различные даты, как валидные, так и невалидные.
+
     }
 
     @Test
@@ -37,17 +31,20 @@ public class OrderingCardDelivery {
     void completedForm() {
         Configuration.holdBrowserOpen = true;
 
+        String planningDate = generateDate(4); // тестовый метод для работы с датой
+
+
         $("[data-test-id=city] input").setValue("Красноярск");
         $("[data-test-id=date] .input__control").click();
         $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] .input__control").sendKeys(BACK_SPACE);
-        $("[data-test-id=date] .input__control").setValue(getDate());
+        $("[data-test-id=date] .input__control").setValue(planningDate);
         $("[data-test-id=name] .input__control").setValue("Иван Васильев");
         $("[data-test-id=phone] .input__control").setValue("+79135178898");
         $("[data-test-id=agreement]").click();
         $x("//button[contains(@class, 'button_view_extra')]").click();
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + getDate()));
+        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 
 }
